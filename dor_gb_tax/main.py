dd51cca12369a71fcf8a6408c3496bedb29e39aa
Ben Ennis <nwbenennis@gmail.com>
2017-06-08 22:54:25 -0700

    Add month to quarter map

--- dor_gb_tax/main.py
+++ dor_gb_tax/main.py
@@ -9,6 +9,8 @@ SHEETS = ['Leaves', 'Flowers', 'Immature Plants', 'Edibles', 'Concentrates',
 FIELDS = ['Date of Sale', 'Amount Sold', 'Exempt Amount Sold', 'Total Sales',
           'Tax-Exempt Sales', 'Taxable Sales']
 
+QUARTERS = {1: 1, 2: 1, 3: 1, 4: 2, 5: 2, 6: 2, 7: 3, 8: 3, 9: 3, 10: 4, 11: 4,
+            12: 4}
 QUARTER_STRINGS = ['1st', '2nd', '3rd', '4th']
 
 
